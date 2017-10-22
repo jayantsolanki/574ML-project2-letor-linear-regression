48af1035518e13d2955a10421cbc0b6d3aaf6179
Jayant <jayantjnp@gmail.com>
2017-10-21 20:48:55 -0400

    design matrix completed

--- lib.py
+++ lib.py
@@ -1,17 +1,48 @@
+import numpy as np
 from pylab import plot,show
 from numpy import vstack,array
 from numpy.random import rand
 from scipy.cluster.vq import kmeans,vq
 
-def design_matrix(X):
+#################################
+#function for calculating Kmeans clustering
+#function KMeans(X, K)
+#input : data set, and number of clusters required
+#output : centroids calculated, spreads is the K covariance matrix
+def kMeans(X, K):
+	[N,D]=X.shape
 	print("Performing Kmeans clustering")
-	centroids,_ = kmeans(X,3)
-	print(centroids)
+	centroids,_ = kmeans(X,K)
+	# print ("Printing the centroids found:")
+	# print(centroids)
 	idx,_ = vq(X,centroids)
-	print(idx.shape)
-	plot(X[idx==0,0],X[idx==0,1],'ob',
-	     X[idx==1,0],X[idx==1,1],'or',
-	     X[idx==2,0],X[idx==2,1],'oc',)
-	plot(centroids[:,0],centroids[:,1],'sg',markersize=8)
-	show()
-	print (centroids.shape)
+	spreads=np.zeros((K, D, D))
+	for k in range(0,K):
+		mat = X[np.where(idx==k)[0]]
+		spreads[k,:,:] = np.cov(mat.T)
+		# print (spreads[k,:,:])
+
+	# print(idx.shape)
+	# plot(X[idx==0,0],X[idx==0,1],'ob',
+	#      X[idx==1,0],X[idx==1,1],'or',
+	#      X[idx==2,0],X[idx==2,1],'oc',)
+	# plot(centroids[:,0],centroids[:,1],'sg',markersize=8)
+	# show()
+	# print (centroids.shape)
+	return centroids, spreads
+
+#################################
+#function for calculating Kmeans clustering
+#function design_matrix(X, mu, spreads)
+#input : data set, and number of clusters found, spreads
+#output : design matrix using Gaussian basis function
+def design_matrix(X, mu, spreads):
+	# [N,D]=X.shape
+	# [M,d] = mu.shape
+	# phi=np.zeros((N,M));
+	# for i in range(0,M):
+	# 	Mpx=math.exp(-1*((X-mu[i,:]).dot(np.linalg.inv(spreads[i,:,:])).dot(np.transpose(X[i,:]-mu[i,:])))/2)
+	# # print((x-mu).dot(np.linalg.inv(covarianceMat)).dot(np.transpose(x-mu)))
+	# 	Sum=math.log(Mpx)+Sum
+	basis_func_outputs = np.exp(np.sum(np.matmul(X - mu, spreads) * (X - mu),axis=2) / (-2)).T
+	return np.insert(basis_func_outputs, 0, 1, axis=1)
--- main.py
+++ main.py
@@ -5,11 +5,20 @@ from numpy import vstack,array
 from numpy.random import rand
 from scipy.cluster.vq import kmeans,vq
 from lib import *
+K=3#number of clusters
 syn_input_data = np.loadtxt('input.csv', delimiter=',')
 syn_output_data = np.genfromtxt('output.csv').reshape([-1, 1])
+[N, D]=syn_input_data.shape
 # letor_input_data = np.genfromtxt('datafiles/Querylevelnorm_X.csv', delimiter=',')
 # letor_output_data = np.genfromtxt('datafiles/Querylevelnorm_t.csv', delimiter=',').reshape([-1, 1])
 # print (syn_input_data.shape)
 # print (syn_output_data.shape)
 
-design_matrix(syn_input_data)
+[centroids, spreads]=kMeans(syn_input_data, K)# number of clusters is 3
+# centroids=centroids.reshape([K,1,D])
+centroids = centroids[:, np.newaxis, :]
+syn_input_data = syn_input_data[np.newaxis, :, :]
+print (syn_input_data.shape)
+# print ("Printing the centroids found:")
+# print(centroids.shape)
+phi=design_matrix(syn_input_data, centroids, spreads)
