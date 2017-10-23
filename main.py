80b99921e33881e5aaa7557d33bea3439e045214
swatishr <32084176+swatishr@users.noreply.github.com>
2017-10-23 00:30:11 -0400

    changes

--- main.py
+++ main.py
@@ -10,10 +10,7 @@ import matplotlib.pyplot as plt
 
 K=20#number of clusters
 L2_lambda=0.1
-mode = 2
-
-
-#np.random.shuffle(syn_input_data)
+mode = 1
 
 if mode == 1:
 	X = np.loadtxt('input.csv', delimiter=',')
@@ -23,42 +20,42 @@ elif mode == 2:
 	Y = np.genfromtxt('Querylevelnorm_t.csv', delimiter=',').reshape([-1, 1])
 
 [N, D]  = X.shape
-X_train = X[0:int(0.8*N),:]
+X_Train = X[0:int(0.8*N),:]
 X_Val 	= X[int(0.8*N):int(0.9*N),:]
-X_test  = X[int(0.9*N):N,:]
-Y_train = Y[0:int(0.8*N),:]
-Y_val 	= Y[int(0.8*N):int(0.9*N),:]
-Y_test  = Y[int(0.9*N):N,:]
+X_Test  = X[int(0.9*N):N,:]
+Y_Train = Y[0:int(0.8*N),:]
+Y_Val 	= Y[int(0.8*N):int(0.9*N),:]
+Y_Test  = Y[int(0.9*N):N,:]
 # RANDOMLY SHUFFLE THE DATA BEFORE PERFORMING THE KMEANS CLUSTERING
 # letor_input_data = np.genfromtxt('Querylevelnorm_X.csv', delimiter=',')
 # letor_output_data = np.genfromtxt('Querylevelnorm_t.csv', delimiter=',').reshape([-1, 1])
-# X_train = letor_input_data[0:int(0.8*N),:]
+# X_Train = letor_input_data[0:int(0.8*N),:]
 # X_Val = letor_input_data[int(0.8*N):int(0.9*N),:]
-# X_test = letor_input_data[int(0.9*N):N,:]
-# Y_train = letor_input_data[0:int(0.8*N),:]
-# Y_val = letor_input_data[int(0.8*N):int(0.9*N),:]
-# Y_test  = letor_input_data[int(0.9*N):N,:]
+# X_Test = letor_input_data[int(0.9*N):N,:]
+# Y_Train = letor_input_data[0:int(0.8*N),:]
+# Y_Val = letor_input_data[int(0.8*N):int(0.9*N),:]
+# Y_Test  = letor_input_data[int(0.9*N):N,:]
 # print (syn_input_data.shape)
 # print (syn_output_data.shape)
-# print (X_train.shape)
+# print (X_Train.shape)
 # print (X_Val.shape)
-# print (X_test.shape)
-# print (Y_train.shape)
-# print (Y_val.shape)
-# print (Y_test.shape) 
+# print (X_Test.shape)
+# print (Y_Train.shape)
+# print (Y_Val.shape)
+# print (Y_Test.shape) 
 
-[centroids, spreads]=kMeans(X_train, K)# number of clusters is 3
+[centroids, spreads]=kMeans(X_Train, K)# number of clusters is 3
 # centroids=centroids.reshape([K,1,D])
 centroids = centroids[:, np.newaxis, :]
-X_train = X_train[np.newaxis, :, :]
-print (X_train.shape)
+X_Train = X_Train[np.newaxis, :, :]
+print (X_Train.shape)
 # print ("Printing the centroids found:")
 # print(centroids.shape)
 lowest=100000
 LAMBDA=1000
-design_matrix_train=design_matrix(X_train, centroids, spreads)
-design_matrix_val = design_matrix(X_Val, centroids, spreads)
-design_matrix_test = design_matrix(X_Test, centroids, spreads)
+design_matriX_Train=design_matrix(X_Train, centroids, spreads)
+design_matriX_Val = design_matrix(X_Val, centroids, spreads)
+design_matriX_Test = design_matrix(X_Test, centroids, spreads)
 # W_SGD = sgd_solution(learning_rate=1, minibatch_size=N, num_epochs=200, L2_lambda=0.1, design_matrix=design_matrix, output_data=syn_output_data)
 # print(W)
 
@@ -72,16 +69,16 @@ lam=[]
 fig = plt.figure()
 ax1 = fig.add_subplot(211)
 ax2 = fig.add_subplot(212)
-for l in np.arange(0.001,1,0.001):
-	W_CF=closed_form_sol(l, design_matrix_train, Y_train)
+for l in np.arange(0.1,0.5,0.001):
+	W_CF=closed_form_sol(l, design_matriX_Train, Y_Train)
 	W_CF=W_CF.reshape([K+1,1])
 	# print (W_CF)
-	ErmsTrain= erms(design_matrix_train, Y_train, W_CF, l)
+	ErmsTrain= erms(design_matriX_Train, Y_Train, W_CF, l)
 	Erms_train.append(ErmsTrain)
 	lam.append(l)
-	ErmsVal = erms(design_matrix_val, Y_val, W_CF, l)
+	ErmsVal = erms(design_matriX_Val, Y_Val, W_CF, l)
 	Erms_val.append(ErmsVal)
-	#Y_dash_test = design_matrix_val.dot(W)
+	#Y_dash_test = design_matriX_Val.dot(W)
 
 	if lowest>ErmsVal:
 		lowest=ErmsVal 	
@@ -91,15 +88,25 @@ for l in np.arange(0.001,1,0.001):
 # print(Erms_train)
 # axes = plt.gca()
 # axes.set_ylim([0,1])
-ax1.plot(np.log(lam), Erms_train, 'b-')
-plt.ylabel('Training ERMS)$')
-plt.xlabel('ln(λ))')
-ax2.plot(np.log(lam), Erms_val, 'r-')
-plt.ylabel('Validation ERMS')
-plt.xlabel('ln(λ)')
-# ax3.plot(np.log(lam), Erms_test, 'g-')
-# plt.ylabel('Test ERMS')
+# ax1.plot(np.log(lam), Erms_train, 'b-')
+# plt.ylabel('Training ERMS')
+# plt.xlabel('ln(λ))')
+# ax2.plot(np.log(lam), Erms_val, 'r-')
+# plt.ylabel('Validation ERMS')
 # plt.xlabel('ln(λ)')
-print("Min Erms in Valdiation Set is = %0.4f " %lowest)
-print("At lambda is = %0.4f " %LAMBDA)
+# # ax3.plot(np.log(lam), Erms_test, 'g-')
+# # plt.ylabel('Test ERMS')
+# # plt.xlabel('ln(λ)')
+# print("Min Erms in Valdiation Set is = %0.4f " %lowest)
+# print("At lambda is = %0.4f " %LAMBDA)
+# plt.show()
+
+#Plot graph
+plt.subplot(1, 1, 1)
+plt.plot(np.log(lam), Erms_train, label='Training')
+plt.plot(np.log(lam), Erms_val, label='Validation')
+#plt.vlines(alpha_optim, plt.ylim()[0], np.max(Erms_val), color='k', linewidth=3, label='Optimum on test')
+plt.legend()
+plt.xlabel('ln(λ)')
+plt.ylabel('Root Mean Square Error')
 plt.show()
